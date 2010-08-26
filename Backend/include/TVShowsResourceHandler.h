802d1d2e6a9a5c038415548da9cf17a4bdb39124
Mike <michel.boto@gmail.com>
2010-08-26 20:09:36 +0200

    implemented all other missing resources for viewing by filter

--- Backend/include/TVShowsResourceHandler.h
+++ Backend/include/TVShowsResourceHandler.h
@@ -7,5 +7,6 @@ public:
   virtual void initTestData();
 protected:
   virtual std::string listStatement() const;
-
+  virtual SanitizedParams sanitizeQueryParams(const pion::net::HTTPTypes::QueryParams& dirtyParams) const;
+  virtual std::string viewStatement() const;
 };
