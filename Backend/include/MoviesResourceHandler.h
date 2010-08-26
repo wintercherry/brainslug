faaddd5168de6de1990c308b5c942c58eed14ae7
Mike <michel.boto@gmail.com>
2010-08-26 19:50:37 +0200

    added view support for movies

--- Backend/include/MoviesResourceHandler.h
+++ Backend/include/MoviesResourceHandler.h
@@ -6,6 +6,7 @@ public:
   MoviesResourceHandler(const DBPtr db);
   virtual void initTestData();
 protected:
+  virtual SanitizedParams sanitizeQueryParams(const pion::net::HTTPTypes::QueryParams& dirtyParams) const;
   virtual std::string listStatement() const;
-
+  virtual std::string viewStatement() const;
 };
