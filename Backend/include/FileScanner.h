32b2b266088739ac5ac7cde031deb59eeb377649
Mike <michel.boto@gmail.com>
2010-08-27 20:02:48 +0200

    stubs for adding sources

--- Backend/include/FileScanner.h
+++ Backend/include/FileScanner.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <boost/filesystem.hpp>
+
+namespace bfs = boost::filesystem;
 
 class FileScanner {
 public:
 	void run();
 	void join();
+	void addTVShowsSourcePath(const bfs::path& sourcePath);
+	void addMoviesSourcePath(const bfs::path& sourcePath);
 };
