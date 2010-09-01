c4f8aaf781b4cc4eec1195a96e935d63d7a7d4f1
Mike <michel.boto@gmail.com>
2010-09-02 00:02:50 +0200

    naive implementation of file scanning

--- Backend/include/FileScanner.h
+++ Backend/include/FileScanner.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Theron/Actor.h>
 #include <boost/filesystem.hpp>
+#include <set>
 #include "FrontendServerMessages.h"
 #include "CommonMessages.h"
 
@@ -17,5 +18,13 @@ private:
   void handleFrontendServerMessage(const FrontendServerMessage& fsm, const Theron::Address from);
   void handleRunMessage(const RunMessage& rm, const Theron::Address from);
   void handleJoinMessage(const JoinMessage& rm, const Theron::Address from);
-  Theron::Address _frontendServer;
+  void launchTVAgents();
+  void launchMovieAgents();
+  void launchTVAgentForPath(const bfs::path& p);
+  void launchMovieAgentForPath(const bfs::path& p);
+  void stopTVAgents();
+  void stopMovieAgents();
+  Theron::Address _frontendServer, _joinReceiver;
+  std::stack<bfs::path> _tvPaths, _moviePaths;
+  std::set<bfs::path::string_type> _supportedExtensions;
 };
--- Backend/lib/FileScanner.cpp
+++ Backend/lib/FileScanner.cpp
@@ -1,4 +1,5 @@
 #include <Theron/Framework.h>
+#include <iostream>
 #include "FileScanner.h"
 
 FileScanner::FileScanner() 
@@ -6,12 +7,71 @@ FileScanner::FileScanner()
   RegisterHandler(this, &FileScanner::handleFrontendServerMessage);
   RegisterHandler(this, &FileScanner::handleRunMessage);
   RegisterHandler(this, &FileScanner::handleJoinMessage);
+  _supportedExtensions.insert(".avi");
+  _supportedExtensions.insert(".mp4");
+  _supportedExtensions.insert(".mov");
+  const bfs::path testPath("/Volumes/Disk1/TV");
+  addTVShowsSourcePath(testPath);
+}
+
+void FileScanner::addTVShowsSourcePath(const bfs::path& sourcePath) {
+  _tvPaths.push(sourcePath);
+}
+
+void FileScanner::addMoviesSourcePath(const bfs::path& sourcePath) {
+  _moviePaths.push(sourcePath);
 }
 
 void FileScanner::run() {
+  launchTVAgents();
+  launchMovieAgents();
 }
 
 void FileScanner::join() {
+  stopTVAgents();
+  stopMovieAgents();
+}
+
+void FileScanner::launchTVAgents() {
+   while (!_tvPaths.empty()) {
+    const bfs::path& tvPath = _tvPaths.top();
+    _tvPaths.pop();
+    launchTVAgentForPath(tvPath);
+  }
+}
+
+namespace {
+  const bfs::directory_iterator dirEnd;
+}
+
+void FileScanner::launchTVAgentForPath(const bfs::path& p) {
+  if (bfs::exists(p)) {
+    bfs::directory_iterator dirIt(p);
+    for (; dirIt!=dirEnd; ++dirIt) {
+      if (bfs::is_directory(dirIt->status())) {
+	launchTVAgentForPath(dirIt->path());
+      } else if (_supportedExtensions.find(dirIt->path().extension())!=_supportedExtensions.end()) {
+	std::cout << "Located matching file: " << dirIt->path().file_string() << std::endl;
+      }
+    }
+  }
+}
+
+void FileScanner::launchMovieAgents() {
+  while (!_moviePaths.empty()) {
+    const bfs::path& moviePath = _moviePaths.top();
+    _moviePaths.pop();
+    launchMovieAgentForPath(moviePath);
+  }
+}
+
+void FileScanner::launchMovieAgentForPath(const bfs::path& p) {
+}
+
+void FileScanner::stopTVAgents() {
+}
+
+void FileScanner::stopMovieAgents() {
 }
 
 void FileScanner::handleFrontendServerMessage(const FrontendServerMessage& fsm, const Theron::Address from) {
@@ -19,8 +79,10 @@ void FileScanner::handleFrontendServerMessage(const FrontendServerMessage& fsm, 
 }
 
 void FileScanner::handleJoinMessage(const JoinMessage& jm, const Theron::Address from) {
+  std::cout << "Joining message received" << std::endl;
   join();
-  GetFramework().Send(JoinFinishedMessage(), GetAddress(), from);
+  _joinReceiver = from;
+  GetFramework().Send(JoinFinishedMessage(), GetAddress(), _joinReceiver);
 }
 
 void FileScanner::handleRunMessage(const RunMessage& rm, const Theron::Address from) {
